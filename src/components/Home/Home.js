70362ddad878911f27bfb09d94c63f7ea6521f8b
Abu Siam <siamcse01@gmail.com>
2022-10-24 00:37:22 +0600

    react slick added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import HomeLeftSide from './HomeLeftSide/HomeLeftSide';
 import HomeRightSide from './HomeRightSide/HomeRightSide';
 
@@ -7,34 +10,47 @@ const Home = () => {
     const hotels = useLoaderData();
     const [selectedPlace, setSelectedPlace] = useState(hotels[0]);
 
-    const handleTourPlace = (id) =>{
+    const settings = {
+        focusOnSelect: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 500
+    };
+
+    const handleTourPlace = (id) => {
         console.log(id);
         let selectHotel;
-        if(id){
+        if (id) {
             selectHotel = hotels.find(hotel => hotel.id === id);
             console.log(hotels[0]);
             console.log(selectHotel);
         }
-        
+
         setSelectedPlace(selectHotel);
     }
     console.log(selectedPlace);
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)),url("${selectedPlace?.img}")`}}>
+        <div className="hero min-h-screen" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)),url("${selectedPlace?.img}")` }}>
             <div className='grid grid-cols-2 mt-40'>
                 <div className=''>
                     {
                         <HomeLeftSide selectedPlace={selectedPlace} ></HomeLeftSide>
                     }
                 </div>
-                <div className='grid grid-cols-3 gap-6 mr-4'>
-                    {
-                        hotels.map(hotel=><HomeRightSide
-                            key={hotel.id}
-                            hotel={hotel}
-                            handleTourPlace={handleTourPlace}
-                        ></HomeRightSide>)
-                    }
+                <div className='mr-10'>
+                    <Slider {...settings}>
+                        
+                            {
+                                hotels.map(hotel => <HomeRightSide
+                                    key={hotel.id}
+                                    hotel={hotel}
+                                    handleTourPlace={handleTourPlace}
+                                ></HomeRightSide>)
+                            }
+
+                        
+                    </Slider>
                 </div>
             </div>
         </div>
--- src/components/Home/HomeRightSide/HomeRightSide.js
+++ src/components/Home/HomeRightSide/HomeRightSide.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
+
 const HomeRightSide = ({ hotel, handleTourPlace }) => {
     const { id, name, img } = hotel;
+
+    
+
     return (
-        <div onClick={() => handleTourPlace(id)} className="card w-full h-80 bg-base-100 shadow-xl image-full cursor-pointer">
+        <div onClick={() => handleTourPlace(id)} className="card w-3/4 mx-auto h-80 bg-base-100 shadow-xl image-full cursor-pointer">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
             </div>
         </div>
+        
     );
 };
 
